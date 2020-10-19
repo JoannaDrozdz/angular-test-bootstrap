aaccbc6db1287adf19fd74736b256f2fc4efa6df
Emil Bartoszczyk <imeil.007@gmail.com>
2020-10-19 21:47:21 +0200

    remove string type in navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  navTitle = 'Angular test bootstrap';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  goToHome(): void {
+    this.navTitle = 'Angular test bootstrap';
+  }
+
+  goToFeatures(): void {
+    this.navTitle = 'Features page';
   }
 
 }
