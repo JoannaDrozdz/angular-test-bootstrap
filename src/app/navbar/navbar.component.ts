f882c7a6022b5bbe38a360b37ed7e4172012c0ac
Emil Bartoszczyk <imeil.007@gmail.com>
2020-10-19 22:45:09 +0200

    add search input

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -18,4 +18,7 @@ export class NavbarComponent implements OnInit {
   goToFeatures(): void {
     this.navTitle = 'Features page';
   }
+  getData(event: any): void {
+    console.log(event);
+  }
 }
