45797aba7883dd6b6767e25214e4d82371e000f0
Emil Bartoszczyk <imeil.007@gmail.com>
2020-10-19 22:13:11 +0200

    add prettier to project and fix styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
   navTitle = 'Angular test bootstrap';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goToHome(): void {
     this.navTitle = 'Angular test bootstrap';
   }
-
   goToFeatures(): void {
     this.navTitle = 'Features page';
   }
-
 }
