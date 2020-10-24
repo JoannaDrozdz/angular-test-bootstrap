c73902bfd9388514780829bc353ab59dd84fb99a
Emil Bartoszczyk <imeil.007@gmail.com>
2020-10-24 21:48:11 +0200

    add ngFor and inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testowa apka';
+
+  movie = {
+    title: 'Szklana pułapka 1',
+    description: 'Lorem ipsum',
+    year: '2000r',
+  };
+
+  movies = [
+    {
+      title: 'Szklana pułapka 1',
+      description: 'Lorem ipsum',
+      year: '2000r',
+    },
+    {
+      title: 'Szklana pułapka 2',
+      description: 'Lorem ipsum',
+      year: '2002r',
+    },
+    {
+      title: 'Szklana pułapka 3',
+      description: 'Lorem ipsum',
+      year: '2003r',
+    },
+  ];
 }
