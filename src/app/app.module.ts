c933c1386189745dfa017d1972be7620184b575c
Emil Bartoszczyk <imeil.007@gmail.com>
2020-11-06 22:11:28 +0100

    Add directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,20 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MovieElementComponent } from './movie-element/movie-element.component';
 import { MovieInputComponent } from './movie-input/movie-input.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MovieElementComponent,
-    MovieInputComponent
+    MovieInputComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
-  imports: [
-    BrowserModule,
-    ButtonsModule,
-  ],
+  imports: [BrowserModule, ButtonsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
