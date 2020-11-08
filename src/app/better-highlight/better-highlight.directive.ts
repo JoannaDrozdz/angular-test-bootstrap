351d263451c1439214a97185ac41e8490d8dd2e2
Joanna <joanna.l.drozdz@gmail.com>
2020-11-08 22:51:17 +0100

    add HostBinding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   OnInit,
   Renderer2,
@@ -10,25 +11,32 @@ import {
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.color') textColor: string;
+
   constructor(private elToChange: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.elToChange.nativeElement, 'background-color', 'gray');
+    this.textColor = '#212529';
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event): void {
-    this.renderer.setStyle(
-      this.elToChange.nativeElement,
-      'background-color',
-      'gray'
-    );
+    //   this.renderer.setStyle(
+    //     this.elToChange.nativeElement,
+    //     'background-color',
+    //     'gray'
+    //   );
+    this.backgroundColor = 'gray';
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event): void {
-    this.renderer.setStyle(
-      this.elToChange.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    this.backgroundColor = 'transparent';
+    this.textColor = '#212529';
+  }
+
+  @HostListener('click') mouseClick(eventData: Event): void {
+    this.backgroundColor = this.backgroundColor === 'red' ? 'gray' : 'red';
+    this.textColor = this.textColor === '#212529' ? 'white' : '#212529';
   }
 }
